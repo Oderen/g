1877734d9902f92defa7ea12a406568da85dab4e
Oderen <oleksandr.deren02@gmail.com>
2023-03-23 14:17:48 +0200

    p

--- src/test.js
+++ src/test.js
@@ -1,5 +1,5 @@
-export default function fetchCountries(name) {
-   return fetch(`https://restcountries.com/v3.1/all?fields=name,population,capital,languages,flags`)
-           .then(response => response.json())
-        .catch(error => console.log(error));
-};
+// export default function fetchCountries(name) {
+//    return fetch(`https://restcountries.com/v3.1/all?fields=name,population,capital,languages,flags`)
+//            .then(response => response.json())
+//         .catch(error => console.log(error));
+// };
